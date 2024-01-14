3014c5b7ae011dadc9103ebe3eb7d92919b404a2
YQh9Mh3181jzEJ5 <mfghz487k44o6u6ekgub@gmail.com>
2024-01-14 16:32:28 +0900

    add

--- index.js
+++ index.js
@@ -3,6 +3,11 @@ const onClickAdd = () => {
   const inputText = document.getElementById("add-text").value;
   document.getElementById("add-text").value = "";
 
+  createIncompleteTodo(inputText);
+};
+
+// 渡された引数を元に未完了のToDoを作成する関数
+const createIncompleteTodo = (todo) => {
   // liタグ生成
   const li = document.createElement("li");
 
@@ -13,10 +18,42 @@ const onClickAdd = () => {
   // pタグ生成
   const p = document.createElement("p");
   p.className = "todo-item";
-  p.innerText = inputText;
+  p.innerText = todo;
+
+  // button(完了)タグ生成
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    // 押された完了ボタンの親タグ(li)配下の完了ボタンと削除ボタンを削除
+    const moveTarget = completeButton.closest("li");
+    completeButton.nextElementSibling.remove();
+    completeButton.remove();
+    // 戻すボタンを生成する
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      const todoText = backButton.previousElementSibling.innerText;
+      createIncompleteTodo(todoText);
+      backButton.closest("li").remove();
+    });
+    moveTarget.firstElementChild.appendChild(backButton);
+    // 完了リストに移動
+    document.getElementById("complete-list").appendChild(moveTarget);
+  });
+
+  // button(削除)タグ生成
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    // 押された削除ボタンの親タグ(li)を未完了リストから削除
+    const deleteTarget = deleteButton.closest("li");
+    document.getElementById("incomplete-list").removeChild(deleteTarget);
+  });
 
   // liタグの子要素に各要素を設定
   div.appendChild(p);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
   li.appendChild(div);
 
   // 未完了リストに追加
